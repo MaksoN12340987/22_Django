83cae94e80c9709a65ffff35e209b05fa92de9f6
Maksim Andreev <andrianov_maksim@outlook.com>
2025-05-18 10:05:58 +0300

    Addd html documents to view info and form, config django

---
+++ first_project/views.py
@@ -0,0 +1,31 @@
+import logging
+
+
+logger_views = logging.getLogger(__name__)
+file_handler = logging.FileHandler(f"log/{__name__}.log", mode="w", encoding="UTF8")
+file_formatter = logging.Formatter(
+    "\n%(asctime)s %(levelname)s %(name)s \n%(funcName)s %(lineno)d: \n%(message)s", datefmt="%H:%M:%S %d-%m-%Y"
+)
+file_handler.setFormatter(file_formatter)
+logger_views.addHandler(file_handler)
+logger_views.setLevel(logging.INFO)
+
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def about(request):
+    logger_views.debug(request)
+    return render(request, 'first_project/about.html')
+
+def contact(request):
+    logger_views.debug(request)
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        message = request.POST.get("message")
+        
+        return HttpResponse(f"Спасибо, {name}! Сообщение отправлено.")
+    return render(request, 'first_project/contact.html')
