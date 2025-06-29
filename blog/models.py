4075af07c21e97b22b8e1a0b18329c22ab88b356
Maxim Andreev <andrianov_maksim@outlook.com>
2025-06-29 13:24:17 +0300

    blog Posts preview upload_to='/media/'

--- blog/models.py
+++ blog/models.py
@@ -3,20 +3,16 @@ from typing import Any
 from django.db import models  # type: ignore
 
 # from PIL import Image # type: ignore
-from config.settings import MEDIA_ROOT  # type: ignore
+from config.settings import MEDIA_ROOT
 
 
 class Posts(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок", unique=True)
     content = models.TextField(null=True, blank=True, verbose_name="Содержимое")
     preview = models.ImageField(
-<<<<<<< HEAD
-        upload_to="blog/", verbose_name="Фотография", null=True, blank=True
-=======
-        upload_to=MEDIA_ROOT,
+        upload_to="/media/",
         verbose_name="Фотография",
         null=True,
->>>>>>> b515b74ecb50180b82f5739d525e6d1da1c0d391
     )
     creation_date = models.DateTimeField(
         auto_now_add=True, verbose_name="Дата создания"
